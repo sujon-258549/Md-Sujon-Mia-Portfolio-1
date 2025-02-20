88b66309f5a3583967a113ffa339fa2caa7bb355
Md Sujon Mia <sujon258549@gmail.com>
2025-02-21 02:11:04 +0600

    modified Project.tsx in home

--- src/app/components/home/Project.tsx
+++ src/app/components/home/Project.tsx
@@ -10,7 +10,7 @@ const Project = () => {
           className="
           text-3xl text-white font-bold  md:text-5xl text-center "
         >
-          Our Project
+          Project Section
         </h1>
         <div className=" max-w-[80%] md:max-w-[500px] border-b md:border-b-[2px] border-white mx-auto mt-3 md:mt-5"></div>
       </div>
